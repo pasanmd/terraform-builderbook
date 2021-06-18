2b3750c2b4dd28ca4ed3da104b45ff062b5bb0e8
Pasan Maddage <pasanmd@gmail.com>
2021-06-19 00:20:35 +0530

    added new terraform script

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -1,48 +1,29 @@
 provider "aws" {
-        version    = "~> 2.0"
-        access_key = var.access_key
-        secret_key = var.secret_key
-        region     = var.region
+        region     = "us-east-1"
 }
 # create an instance
-resource "aws_instance" "linux_instance" {
-  ami             = lookup(var.amis, var.region)
-  subnet_id       = var.subnet
-  security_groups = var.securityGroups
-  key_name        = var.keyName
-  instance_type   = var.instanceType
-  # Let's create and attach an ebs volume
-  ebs_block_device {
-    device_name = "/dev/xvdb"
-    volume_type = "gp2"
-    volume_size = 8
-  }
-  # Name the instance
-  tags = {
-    Name = var.instanceName
-  }
 
-  volume_tags = {
-    Name = var.instanceName
-  }
-  # Copy in the bash script we want to execute.
-  provisioner "file" {
-    source      = "~/setup-builderbook"
-    destination = "/tmp/setup-builderbook"
-  }
-  # Change permissions on bash script and execute from ec2-user.
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/setup-builderbook",
-      "sudo /tmp/setup-builderbook",
-    ]
-  }
-  # Login to the ec2-user with the aws key.
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    password    = ""
-    private_key = file(var.keyPath)
-    host        = self.public_ip
-  }
+variable cidr_blocks {}
+variable subnet_idr_blocks {}
+variable avail_zone {}
+variable env_prefix {}
+
+#create the vpc
+
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block= var.vpc_cidr_block
+    tags = {
+        Name: "${var.env_prefix}-vpc"
+    }
 }
+
+#create the subnet
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cid_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
+    tag = {
+        Name: "${var.env_prefix}-subnet-1}"
+    }
+}
+
